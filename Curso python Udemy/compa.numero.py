e3a4ed89ca07dbdf00374f3c67fe2866695b3683
Marlon Santiago <pcdmrl@gmail.com>
2025-01-27 12:14:57 -0300

    conferindo se uma variavel são iguais

---
+++ Curso python Udemy/compa.numero.py
@@ -0,0 +1,12 @@
+valor1 = input('Digite um valor: ')
+valor2 = input('Digite outro valor: ')
+
+if valor1 > valor2:
+    print(f'O primeiro valor {valor1} e maior que o segundo valor {valor2}')
+
+elif valor2 > valor1:
+    print(f'O segundo valor {valor2} é maior que o primeiro valor {valor1}')
+
+
+
+
