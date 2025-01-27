ec38738685854a6f88e2eee5437de7f07c7e7fa3
Marlon Santiago <pcdmrl@gmail.com>
2025-01-27 12:16:17 -0300

    conferindo as variaveis e confirmando se o valor digitado foi apenas números

--- Curso python Udemy/compa.numero.py
+++ Curso python Udemy/compa.numero.py
@@ -8,5 +8,18 @@ elif valor2 > valor1:
     print(f'O segundo valor {valor2} é maior que o primeiro valor {valor1}')
 
 
+numero1 = input('Digite um número: ')
+numero2 = input('Digite outro numero: ')
 
+if numero1 > numero2 and numero1.isdigit() and numero2.isdigit():
+    print(f'O numero {numero1} e maior que o número {numero2}')
+
+elif numero2 > numero1 and numero2.isdigit() and numero1.isdigit():
+    print(f'O número {numero2} é maior que o número {numero1}')
+
+elif numero1 == numero2 and numero1.isdigit() and numero2.isdigit():
+    print(f'Os números são iguais')
+
+else:
+    print(f'por favor digite apenas números')
 
