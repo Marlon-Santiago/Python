5caacd5c40a94276d5fadb3383ed97dead2d3842
Marlon Santiago <pcdmrl@gmail.com>
2025-01-27 14:00:58 -0300

    Adicionando o operador logico not

--- Curso python Udemy/compa.numero.py
+++ Curso python Udemy/compa.numero.py
@@ -7,8 +7,12 @@ if valor1 > valor2:
 elif valor2 > valor1:
     print(f'O segundo valor {valor2} é maior que o primeiro valor {valor1}')
 
+elif not valor1 and not valor2:
+    print(f'Você não digitou nada preencha os campos corretamenta')
+
 else:
     print(f'Os valores são iguais')
+
 
 
 numero1 = input('Digite um número: ')
@@ -22,6 +26,9 @@ elif numero2 > numero1 and numero2.isdigit() and numero1.isdigit():
 
 elif numero1 == numero2 and numero1.isdigit() and numero2.isdigit():
     print(f'Os números são iguais')
+
+elif not numero1 and not numero2:
+    print(f'Você não preencheu os campos com números')
 
 else:
     print(f'por favor digite apenas números')
