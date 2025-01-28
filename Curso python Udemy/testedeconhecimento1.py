2ac53e58fc7ceda9ee17725dd073266cf5ba30ef
Marlon Santiago <pcdmrl@gmail.com>
2025-01-28 11:10:34 -0300

    teste de conhecimento com oque aprendi ate agora.

---
+++ Curso python Udemy/testedeconhecimento1.py
@@ -0,0 +1,17 @@
+nome = input('Digite seu nome: ')
+
+idade = input('Digite sua idade: ')
+
+if nome and idade :
+    print(f'O seu nome é {nome} e sua idade é {idade}')
+    print(f'Esse é o seu nome invertido {nome[::-1]}')
+    print(f'Seu nome tem espaços {' ' in nome}')
+    print(f'Seu nome tem {len(nome)} letras')
+    print(f'A primeira letra do seu nome é {nome[0]}')
+    print(f'A ultima letra do seu nome é {nome[-1]}')
+
+
+else:
+    not nome or not idade
+    print('Desculpe você deixou um dos campos vazios')
+
