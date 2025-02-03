b8b59a68f90d7e9a4c8b67443779be7cf57235b7
Marlon Santiago <pcdmrl@gmail.com>
2025-02-03 09:49:05 -0300

    mudei o codigo da linha 17 e adicionei como comentario a correção do professor

--- Curso python Udemy/Exercicios/teste_de_conhecimento3.py
+++ Curso python Udemy/Exercicios/teste_de_conhecimento3.py
@@ -14,7 +14,7 @@ segundos = input('Digie os segundos: ')
 if hora.isdigit() and minutos.isdigit() and minutos.isdigit():
     hora = int(hora)
     
-    if hora <= 11:
+    if hora >= 0 and hora <= 11:
         print(f'Bom Dia !!!!!, São {hora}:{minutos}:{segundos}')
 
     elif hora > 11 and hora <= 17:
@@ -27,6 +27,26 @@ if hora.isdigit() and minutos.isdigit() and minutos.isdigit():
         print('O dia tem 24 horas os minutos vão ate 59 assim como os segundos digite os numeros dentro desses valores')
 
 else:
-    print('Você não digitou um número válido')
+    print('Você não digitou uma hora válida')
+
+'''
+CORREÇÃO DO PROFESOR
+
+entrada = input('Digite a hora em números inteiros: ')
+
+try:
+    hora = int(entrada)
+
+    if hora >= 0 and hora <= 11:
+        print('Bom dia')
+    elif hora >= 12 and hora <= 17:
+        print('Bom tarde')
+    elif hora >= 18 and hora <= 23:
+        print('Bom noite')
+    else:
+        print('Não conheço essa hora')
+except:
+    print('Por favor, digite apenas números inteiros')
+'''
 
 
