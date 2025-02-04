34ef4d8155b5092edfdde52c2e9257f7501c5bbb
Marlon Santiago <pcdmrl@gmail.com>
2025-02-04 11:34:10 -0300

    Aula inicial sobre  While

---
+++ Curso python Udemy/Aulas.py/repetiçoes-while.py
@@ -0,0 +1,19 @@
+"""
+Repetições
+while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+Loop infinito -> Quando um código não tem fim
+
+"""
+
+condicao = True
+
+while condicao:
+    nome = input('Digite seu nome: ')
+    print(f'Seu nome é {nome}')
+
+    if nome == 'sair':
+        print(f' Você escolheu sair')
+        break
+
+print('PAROOOOOOOOOOOOOOOOOOOOOOOOOOOOO')
