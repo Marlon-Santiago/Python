25390bd5cdd5b3a2934f15f43cfdfb290acc9d2f
Marlon Santiago <pcdmrl@gmail.com>
2025-02-04 12:07:13 -0300

    parando  o while tornando a variavel False

---
+++ Curso python Udemy/Aulas.py/while-ate-a-condicao-falsa.py
@@ -0,0 +1,34 @@
+
+"""
+Repetições
+while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+Loop infinito -> Quando um código não tem fim
+
+"""
+contador = 0
+
+''' 
+while contador <= 10: # O contador vai até 11
+    contador = contador + 1
+    print(contador)
+
+print('acabou')
+
+'''
+
+'''
+
+while contador < 10: # O contador vai até 10
+    contador = contador + 1
+    print(contador)
+
+print('acabou')
+
+'''
+
+while contador < 10: # contardo vai ate 9
+    print(contador)
+    contador = contador + 1
+
+print('acabou')
