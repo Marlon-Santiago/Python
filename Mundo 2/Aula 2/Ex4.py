21d8a815e1beb3f0a7e61b1b5372a22d212aea11
Marlon Santiago <pcdmrl@gmail.com>
2025-04-17 17:14:40 -0300

    ...

--- Mundo 2/Aula 2/Ex4.py
+++ Mundo 2/Aula 2/Ex4.py
@@ -1,8 +1,8 @@
 numero = int(input('digite um numero eu mostrarei a tabuada de 1 a 10: '))
 
-contado  = 0
+contador  = 0
 
 for i in range(0, 11):
-    resultado = numero * contado
-    print(f'o resultado de {numero} x {contado} é {resultado}')
-    contado = contado + 1
+    resultado = numero * contador
+    print(f'o resultado de {numero} x {contador n } é {resultado}')
+    contador = contador + 1
---
+++ Mundo 2/Aula 2/Ex5.py
@@ -0,0 +1,8 @@
+numero1 = int(input('Digite um numero inteiro:'))
+numero2 = int(input('Digite um numero inteiro:'))
+numero3 = int(input('Digite um numero inteiro:'))
+numero4 = int(input('Digite um numero inteiro:'))
+numero5 = int(input('Digite um numero inteiro:'))
+numero6 = int(input('Digite um numero inteiro:'))
+
+
