923edd6465e772d6e6e4691378a1daa3ec37e197
Marlon Santiago <pcdmrl@gmail.com>
2025-05-14 10:43:37 -0300

    Exercicio 52 curso em video
     Faça um programa que leia um número inteiro e diga se ele é ou não um número primo.

---
+++ Mundo 2/Aula 2/Ex7.py
@@ -0,0 +1,14 @@
+num = int(input('Digite um número : '))
+
+primo = 0
+
+for c in range(1, num+1):
+    if num % c == 0:
+        primo += 1
+
+if primo == 2:
+    print('Esse numero é primo')
+
+else:
+    print('não primo')
+        
