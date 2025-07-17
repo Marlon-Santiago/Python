e2fca4627b1ba4ea6d8d1eb233ac8c1d04d0e139
Marlon Santiago <pcdmrl@gmail.com>
2025-07-17 10:17:33 -0300

    Exercício Python 063: Escreva um programa que leia um número N inteiro qualquer e mostre na tela os N primeiros elementos de uma Sequência de Fibonacci

--- Mundo 2/Aula 3/Ex6.py
+++ Mundo 2/Aula 3/Ex6.py
@@ -2,7 +2,7 @@
 Exercício Python 062: Melhore o DESAFIO 061, perguntando para o usuário se ele quer mostrar mais alguns termos. 
 O programa encerrará quando ele disser que quer mostrar 0 termos.
 '''
-
+#
 termo = int(input('Digite o preimeiro termo: '))
 razao = int(input('Digite a razão: '))
 
---
+++ Mundo 2/Aula 3/Ex7.py
@@ -0,0 +1,18 @@
+'''
+Exercício Python 063: Escreva um programa que leia um número N inteiro 
+qualquer e mostre na tela os N primeiros elementos de uma Sequência de Fibonacci. 
+'''
+
+termos = int(input('Quantos termos você quer mostrar? '))
+
+fibonacci = [0, 1]  # Iniciando a lista com os dois primeiros termos da sequência de Fibonacci
+
+while len(fibonacci) < termos:
+    proximo_termo = fibonacci[-1] + fibonacci[-2] # Calcula o próximo termo somando os dois últimos termos
+    fibonacci.append(proximo_termo)
+
+print('Sequencia de Fibonacci: ')
+print(fibonacci) # Exibe a sequência de Fibonacci até o número de termos solicitado  
+
+'''Exemplo de uso:
+ '''
